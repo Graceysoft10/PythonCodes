c1336362dca4e0e788abcc1d5c58c91340ab499e
Grace <funmibiajadi@gmail.com>
2021-06-17 22:54:52 +0100

    triangle

---
+++ pythonProject1/PersonalTutorial/Triangle.py
@@ -0,0 +1,10 @@
+def calculatePerimeterOfTriangle(length, width):
+    return 2 * (length + width)
+
+
+def calculateAreaOfTriangle(length, width):
+    return length * width
+
+
+print(f"The perimeter of a Triangle is :{calculatePerimeterOfTriangle(10, 5)}")
+print(f"The Area of a Triangle is :{calculateAreaOfTriangle(10, 5)}")
