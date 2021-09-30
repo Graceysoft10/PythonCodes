99da000c77301a07df85372315389641a6317a22
Grace <funmibiajadi@gmail.com>
2021-09-30 01:10:44 +0100

    I added new stuffs

--- pythonProject1/Chapter5/barchart.py
+++ pythonProject1/Chapter5/barchart.py
@@ -3,3 +3,13 @@ print('\nCreating a bar chart from numbers:')
 print(f'Index{"Value":>10} Bar')
 for index, value in enumerate(numbers):
     print(f' {index :> 5} {value:>5}    {"*" * value}')
+
+numbers2 = list(range(1, 16))
+print(numbers2)
+ev = numbers2[1:len(numbers2):2]
+print(ev)
+numbers2[5:10] = [0] * len(numbers2[5:10])
+
+print(numbers2)
+numbers2[5:] = []
+print(numbers2)
